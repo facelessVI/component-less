f1fa45a7854adad3424378577635b709702714d1
facelessVI <faceless0097@gmail.com>
2025-05-13 20:43:31 -0400

    ai slop template

---
+++ src/app/features/buttons/buttons.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ButtonShowcaseComponent } from './pages/button-showcase/button-showcase.component';
+
+export const BUTTONS_ROUTES: Routes = [
+    {
+        path: '',
+        component: ButtonShowcaseComponent
+    }
+];
